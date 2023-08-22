f209bb15fc83c68e35e7557609d78cdbae7e16a2
RAMPRASADNEVALI1250 <ram.prasad.nevali@gmail.com>
2023-08-22 16:04:19 +0530

    Addedfiles to prectice and some scripts

---
+++ Coding_Questions/1.Create10Files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##############################################
+# Author: User
+# Created Date: date
+# Last Modified: Last modified Date
+#############################################
+
+#Description: Create 10 files 
+
+for i in {1..10}
+do
+	touch file$i
+done
---
+++ Coding_Questions/2.Renamefiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+###################################################
+# Author: User
+# Date: Date
+# Last Modified: Last Modified Date
+###################################################
+
+#Description: Rename all files in a directory(1.Create10files created 10 files, this will add extention .md to them
+
+for i in {1..10}
+do
+	mv file$i file$i.md
+done
+
+
